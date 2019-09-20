4773765fc71fb6f340631cec1a805c7b32febf2f
Elizabeth Tackett <emctackett@gmail.com>
2019-09-20 13:33:52 -0500

    update to OO design

--- snaprazzle.js
+++ snaprazzle.js
@@ -5,97 +5,91 @@ const screen = require('electron').screen || require('electron').remote.screen;
 // wait for page to be ready
 $(() => {
   const display = screen.getPrimaryDisplay();
-  const video   = document.querySelector('video');
-
-  const constraints = {
-    video: {
-      width: {
-        ideal: display.size.width
+  const snaprazzle = {
+    video: document.querySelector('video'),
+    canvas: document.querySelector('canvas'),
+    constraints: {
+      video: {
+          width: {
+            ideal: display.size.width
+          },
+          height: {
+            ideal: display.size.height
+          },
+        }
       },
-      height: {
-        ideal: display.size.height
-      },
-    }
-  }
+    createDir: function(path) {
+      if (!fs.existsSync(path)) {
+        fs.mkdir(path, (err) => { if (err) throw err });
+      }
+    },
+    saveImage: function(path, image) {
+      const imgData = image.replace(/^data:image\/\w+;base64,/, '');
+      const stamp   = new Date().toISOString().replace(/[:.\/]/g, '-');
+      // each photo named with unique timestamp
 
-  // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
-  navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-    const canvas  = document.querySelector('canvas');
-    const desktop = app.getPath('desktop');
-    const path    = `${desktop}/snaprazzle`;
+      fs.writeFile(`${path}/${stamp}.png`, imgData, {encoding: 'base64'}, (err) => {
+        if (err) throw err;
+      });
+    },
+    snapPhoto: function() {
+      const ctx = this.canvas.getContext('2d');
+      if ($(this.video).hasClass('filter')) { ctx.filter = 'grayscale(100%)'; }
 
-    // stream webcam video
-    video.srcObject = stream;
+      // capture image
+      ctx.drawImage(this.video, 0, 0, 500, 300);
+      // reset filter prop
+      ctx.filter = 'none';
 
-    attachEventListeners();
+      $('audio')[0].play();
+      const image   = this.canvas.toDataURL("image/png");
+      const path    = `${app.getPath('desktop')}/snaprazzle`;
 
-  }).catch((error) => {
-    console.error(error);
-  });
+      this.createDir(path);
+      this.saveImage(path, image);
+    },
+    snapMultiPhotos: function(e) {
+      this.snapPhoto();
+
+      e.data.i++;
+
+      // take 3 photos with 1 sec pause between
+      setTimeout(() => {
+        if (e.data.i < 3) {
+          this.snapMultiPhotos(e);
+        } else {
+          e.data.i = 0;
+        }
+      }, 1000);
+    },
+    toggleFilter: function() {
+      if ($('#filter input').is(':checked')) {
+        $(this.video).addClass('filter');
+      } else {
+        $(this.video).removeClass('filter');
+      }
+    },
+    attachEventListeners: function() {
+      $('#snap').on('click', () => { this.snapPhoto() });
+      $('#multi-snap').on('click', {i: 0}, (e) => { this.snapMultiPhotos(e) });
+      $('#filter').on('change', () => { this.toggleFilter() });
+    },
+    init: function() {
+      // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+      navigator.mediaDevices.getUserMedia(this.constraints).then((stream) => {
+        const desktop = app.getPath('desktop');
+        const path    = `${desktop}/snaprazzle`;
+
+        // stream webcam video
+        this.video.srcObject = stream;
+
+        this.attachEventListeners();
+
+      }).catch((error) => {
+        console.error(error);
+      });
+    },
+  };
+
+  snaprazzle.init();
 });
-
-function attachEventListeners() {
-  $('#snap').on('click', snapPhoto);
-  $('#multi-snap').on('click', {i: 0}, snapMultiPhotos);
-  $('#filter').on('change', toggleFilter);
-}
-
-function snapPhoto() {
-  const video   = document.querySelector('video');
-  const canvas  = document.querySelector('canvas');
-
-  ctx = canvas.getContext('2d');
-  if ($(video).hasClass('filter')) { ctx.filter = 'grayscale(100%)'; }
-
-  // capture image
-  ctx.drawImage(video, 0, 0, 500, 300);
-  // reset filter prop
-  ctx.filter = 'none';
-
-  $('audio')[0].play();
-  const image   = canvas.toDataURL("image/png");
-  const path    = `${app.getPath('desktop')}/snaprazzle`;
-
-  createDir(path);
-  saveImg(path, image);
-}
-
-function snapMultiPhotos(e) {
-  snapPhoto();
-
-  e.data.i++;
-
-  // take 3 photos with 1 sec pause between
-  setTimeout(function() {
-    if (e.data.i < 3) {
-      snapMultiPhotos(e);
-    } else {
-      e.data.i = 0;
-    }
-  }, 1000);
-}
-
-function toggleFilter() {
-  if ($('#filter input').is(':checked')) {
-    $('video').addClass('filter');
-  } else {
-    $('video').removeClass('filter');
-  }
-}
-
-function createDir(path) {
-  // if dir doesn't exist, create it
-  if (!fs.existsSync(path)) {
-    fs.mkdir(path, (err) => { if (err) throw err });
-  }
-}
-
-function saveImg(path, image) {
-  const imgData = image.replace(/^data:image\/\w+;base64,/, '');
-  const stamp   = new Date().toISOString().replace(/[:.\/]/g, '-');
-  // each photo named with unique timestamp
-
-  fs.writeFile(`${path}/${stamp}.png`, imgData, {encoding: 'base64'}, (err) => {
-    if (err) throw err;
-  });
-}
